c982e6cb781cd8274b08e58b758dd265b2d25795
Mohammed Hameed <hameed.in@hotmail.com>
2018-05-28 12:03:57 +0530

    Added api url for dev and prod.

--- src/app/employees/shared/employee.service.ts
+++ src/app/employees/shared/employee.service.ts
@@ -2,8 +2,15 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions, RequestMethod } from '@angular/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
 import {Employee} from'./employee.model'
+
+var apiBaseUrl = "http://localhost:50539";
+
+if (environment.production){
+  apiBaseUrl = "http://192.168.1.112/api";
+}
 
 @Injectable()
 export class EmployeeService {
@@ -15,20 +22,20 @@ export class EmployeeService {
     var body = JSON.stringify(emp);
     var headerOptions = new Headers({'Content-Type':'application/json'});
     var requestOptions = new RequestOptions({method : RequestMethod.Post,headers : headerOptions});
-    return this.http.post('http://localhost:50539/api/Employee',body,requestOptions).pipe(map(x => x.json()));
+    return this.http.post(apiBaseUrl + '/api/Employee',body,requestOptions).pipe(map(x => x.json()));
   }
 
   putEmployee(id, emp) {
     var body = JSON.stringify(emp);
     var headerOptions = new Headers({ 'Content-Type': 'application/json' });
     var requestOptions = new RequestOptions({ method: RequestMethod.Put, headers: headerOptions });
-    return this.http.put('http://localhost:50539/api/Employee/' + id,
+    return this.http.put(apiBaseUrl + '/api/Employee/' + id,
       body,
       requestOptions).pipe(map(res => res.json()));
   }
 
   getEmployeeList(){
-    this.http.get('http://localhost:50539/api/Employee')
+    this.http.get(apiBaseUrl + '/api/Employee')
     .pipe(map((data : Response) =>{
       return data.json() as Employee[];
     })).toPromise().then(x => {
@@ -37,6 +44,6 @@ export class EmployeeService {
   }
 
   deleteEmployee(id: number) {
-    return this.http.delete('http://localhost:50539/api/Employee/' + id).pipe(map(res => res.json()));
+    return this.http.delete(apiBaseUrl + '/api/Employee/' + id).pipe(map(res => res.json()));
   }
 }
